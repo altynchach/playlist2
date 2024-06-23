630eb4b3b3594aab8298429652078a37245916f3
altynchach <77624594+altynchach@users.noreply.github.com>
2024-06-23 22:45:11 +0600

    4

--- app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
@@ -1,7 +1,12 @@
 package com.example.playlistmaker
+
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 
 class ActivitySettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +17,34 @@ class ActivitySettings : AppCompatActivity() {
         val arrow = findViewById<ImageView>(R.id.back_button)
         arrow.setOnClickListener {
             finish()
+        }
+
+        // Обработчик для кнопки "Поделиться приложением"
+        val shareAppButton = findViewById<TextView>(R.id.tvShareApp)
+        shareAppButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app)))
+        }
+
+        // Обработчик для кнопки "Написать в поддержку"
+        val supportButton = findViewById<TextView>(R.id.tvSupport)
+        supportButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("your_email@example.com"))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_body))
+            }
+            startActivity(emailIntent)
+        }
+
+        // Обработчик для кнопки "Пользовательское соглашение"
+        val userAgreementButton = findViewById<TextView>(R.id.tvUserAgreement)
+        userAgreementButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(browserIntent)
         }
     }
 }
