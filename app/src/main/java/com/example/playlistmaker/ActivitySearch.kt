26a1088e692f5beb1ebb6af9efe6b3ea0d0e67f6
altynchach <77624594+altynchach@users.noreply.github.com>
2024-06-25 02:30:15 +0600

    8

--- app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
@@ -1,11 +1,26 @@
 package com.example.playlistmaker
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 
 
 class ActivitySearch : AppCompatActivity() {
+
+    private lateinit var inputText: EditText
+    private var restoredText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        inputText = findViewById(R.id.inputEditText)
+        if (savedInstanceState != null) {
+            inputText.setText(restoredText)
+        }
+
+        clearButton = findViewById(R.id.clearIcon)
+
     }
 }
