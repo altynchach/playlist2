76264cc1b325568bb85497662474d80b23d64210
altynchach <77624594+altynchach@users.noreply.github.com>
2024-06-27 04:19:56 +0600

    55

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,13 +5,22 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val SEARCH_QUERY_KEY = "SEARCH_QUERY"
+    }
+
+    private lateinit var searchText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        inputEditText.setTextColor(ContextCompat.getColor(this, R.color.search_text))
         inputEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -20,10 +29,21 @@ class SearchActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 if (s.isNullOrEmpty()) {
                     inputEditText.hint = getString(R.string.search)
+                    inputEditText.setHintTextColor(ContextCompat.getColor(this@SearchActivity, R.color.icon_search))
                 } else {
                     inputEditText.hint = ""
                 }
             }
         })
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_QUERY_KEY, "")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        outState.putString(SEARCH_QUERY_KEY, inputEditText.text.toString())
     }
 }
