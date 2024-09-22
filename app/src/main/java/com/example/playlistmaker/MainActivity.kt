2b4873c22f767b2bcb542c2f33d5b77aa9492178
altynchach <77624594+altynchach@users.noreply.github.com>
2024-09-23 00:22:30 +0600

    3_13

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,25 +3,25 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.util.Log
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("MainActivity", "MainActivity started") // Log the start of the activity
+
         val searchButton = findViewById<Button>(R.id.button_search)
         searchButton.setOnClickListener {
+            Log.d("MainActivity", "Search button clicked") // Log button click
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
 
-        val libraryButton = findViewById<Button>(R.id.button_library)
-        libraryButton.setOnClickListener {
-            // Действие для кнопки библиотеки
-        }
-
         val settingsButton = findViewById<Button>(R.id.button_settings)
         settingsButton.setOnClickListener {
+            Log.d("MainActivity", "Settings button clicked")
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
