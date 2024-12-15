2260a5434395b97c0415883abc092661aae3f50a
altynchach <77624594+altynchach@users.noreply.github.com>
2024-12-15 18:13:01 +0600

    1_18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,5 +63,7 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation ("androidx.fragment:fragment-ktx:1.5.6")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation ("com.google.android.material:material:1.8.0")
 
 }
